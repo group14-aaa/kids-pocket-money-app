7c143caad14622d995c920f7cc90455e08c18e73
adampriley1 <adampriley1@hotmail.com>
2023-12-21 23:19:28 +0000

    altered the code to add values to local storage...added code to retrieve values from local storage, this is not working correctly yet

--- assets/js/taskManager.js
+++ assets/js/taskManager.js
@@ -1,21 +1,55 @@
 $(document).ready(function() {
-const container = $('.container');
-const taskNameInput = $('#task-name-input');
-const taskDescriptionInput = $('#task-description-input');
-const taskValueInput = $('#task-value-input');
+    const container = $('.container');
+    const taskNameInput = $('#task-name-input');
+    const taskDescriptionInput = $('#task-description-input');
+    const taskValueInput = $('#task-value-input');
+
+    // Event listener for submit button. Save values to local storage
+    container.on('click', '.task-submit-button', function() {
+        const currentTime = dayjs();
+        const enteredTask = {
+            name: taskNameInput.val(),
+            description: taskDescriptionInput.val(),
+            value: taskValueInput.val()
+        };
+
+        saveTask(currentTime, enteredTask);
+        loadTasksToTable(currentTime);
+    });
 
 
-container.on('click', '.task-submit-button', function() {
-    const tasks = $([taskNameInput, taskDescriptionInput, taskValueInput]).map(function () {
-       return $(this).val();
-   }).get();
-
-   saveTasks(tasks);
+    //(key = currentTime, value = entered task values)
+    function saveTask(key, task) {
+        localStorage.setItem(key, JSON.stringify(task));
+    }
 });
 
-const currentTime = dayjs();
-function saveTasks(tasks) {
-localStorage.setItem(currentTime, JSON.stringify(tasks));
+// Load tasks to the table in kids dashboard
+
+function getTasksFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key)) || [];
+
 }
 
-});
+function loadTasksToTable(currentTime) {
+  const tasks = getTasksFromLocalStorage(currentTime.format());
+  const tableBody = $('#task-table-body');
+
+  // Clear existing rows
+  tableBody.empty();
+
+  // loop Populate the table with tasks
+  tasks.forEach(function (task, i) {
+    const row = `
+      <tr>
+        <th scope="row">${[i] + 1}</th>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>£${task.value}</td>
+      </tr>
+    `;
+    tableBody.append(row);
+  });
+ 
+}
+
