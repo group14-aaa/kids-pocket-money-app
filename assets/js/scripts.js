953eeec9e681ac47a67372fdaa7a482b1c50253b
adampriley1 <adampriley1@hotmail.com>
2023-12-21 16:24:54 +0000

    used dayJs to add current day and date to kids dashboard

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,4 +13,20 @@ $(document).ready(function() {
     $('#parentRadio').change(function() {
       $('#parentEmailGroup').hide();
     });
+
+    displayDate();
+
+// handle displaying the date
+function displayDate() {
+  setInterval(() => {
+    //get current time
+    const now = dayjs();
+    //format the time
+    const formattedTime = now.format("dddd, MMMM D");
+    //text field displays formatted time
+    $("#display-Day").text(formattedTime);
+  }, 1000);
+}
+
+
   });
