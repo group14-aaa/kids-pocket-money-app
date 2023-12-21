a222ce4f25f530f054fe367bb2a6850b430aa9cb
pmAdriaan <pmAdriaan@users.noreply.github.com>
2023-12-21 17:52:33 +0000

    Changed the login and register to use bootstrap modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,14 @@
-// scripts.js
-//toggle visibility of parent's email field
 $(document).ready(function() {
     // Initial check on page load
     $('#parentEmailGroup').toggle($('#kidRadio').is(':checked'));
 
     // Toggle when Kid radio is changed
-    $('#kidRadio').change(function() {
+    $(document).on('change', '#kidRadio', function() {
       $('#parentEmailGroup').toggle(this.checked);
     });
 
     // Hide when Parent radio is checked
-    $('#parentRadio').change(function() {
+    $(document).on('change', '#parentRadio', function() {
       $('#parentEmailGroup').hide();
     });
-  });
+});
