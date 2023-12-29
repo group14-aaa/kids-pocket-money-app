cbfcefbb0b1fc573f7fafd49681bb9ceae69e4b1
adampriley1 <adampriley1@hotmail.com>
2023-12-29 18:32:43 +0000

    added code to show message in parents dashboard

--- assets/js/withdraw.js
+++ assets/js/withdraw.js
@@ -1,45 +1,33 @@
 $(document).ready(function () {
-
-
-
   // Get the logged-in kid's email
   const currentUser = getCurrentUser();
   const kidEmail = currentUser.email;
-
-    // Event delegation event listener for withdraw button
-    $(document).on("click", ".modal-submit-button", function () {
-
-        //get value of the withdraw field 
-      const withdrawValue = $("#withdraw-value-input").val();
-
-
-          // Get current balance for the kid
-let kidCurrentBalance = localStorage.getItem(`balanceTotal_${kidEmail}`) || 0;
-kidCurrentBalance = JSON.parse(kidCurrentBalance);
-
-kidCurrentBalance = kidCurrentBalance - withdrawValue;
-
-
-    // Store the updated balance for the kid in local storage
-    localStorage.setItem(`balanceTotal_${kidEmail}`, JSON.stringify(kidCurrentBalance));
-
-
-console.log(kidCurrentBalance);
-    
-    });
-
-  });
-
-
+//   const currentParent = currentUser.parentEmail;
   
 
-// calculate withdrawal amount
+  // Event delegation event listener for withdraw button
+  $(document).on("click", ".modal-submit-button", function () {
+    //get value of the withdraw field
+    const withdrawValue = $("#withdraw-value-input").val();
 
-// Update the balance for the kid
-// kidCurrentBalance += taskValue;
+    // Get current balance for the kid
+    let kidCurrentBalance =
+      localStorage.getItem(`balanceTotal_${kidEmail}`) || 0;
+    kidCurrentBalance = JSON.parse(kidCurrentBalance);
 
-// Store the updated balance for the kid in local storage
-// localStorage.setItem(`balanceTotal_${kidEmail}`, JSON.stringify(kidCurrentBalance));
+    kidCurrentBalance = kidCurrentBalance - withdrawValue;
 
-// // Update the global kidBalance variable
-// kidBalance = kidCurrentBalance;
+    // Store the updated balance for the kid in local storage
+    localStorage.setItem(
+      `balanceTotal_${kidEmail}`,
+      JSON.stringify(kidCurrentBalance)
+    );
+
+    //display message on parents dashboard that kid email has withdrawn funds
+    $(".withdrawMessage").text("Your child has withdrawn " + withdrawValue);
+  
+    //add button "funds paid", that clears message from dashboard
+    $(".withdrawMessage").append("<button class='fundsPaidButton'>Funds Paid</button>");
+
+  });
+});
